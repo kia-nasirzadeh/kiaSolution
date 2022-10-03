43d8be5ce1e25125bb45ae47382bcd43e269a0ac
unknown <ashkan.codecman.01@gmail.com>
2022-10-03 15:52:25 +0330

    robot v2 acomplished

---
+++ 2-memoryCardAppV1-consoleApp/memoryCardAppV2-WpfApp/WpfApp1/FlashCardApp/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace WpfApp1.FlashCardApp
+{
+    [Table("AppSettings")]
+    public class AppSettings
+    {
+        [Column("ID"), PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Column("qbDir"), NotNull]
+        public bool QbDir { get; set; }
+        [Column("abDir"), NotNull]
+        public bool AbDir { get; set; }
+    }
+}
